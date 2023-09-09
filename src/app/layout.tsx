95d83a2953f9bbd3bab07d5cc95e85eeec433412
matsubrasil <matsuboy@outlook.com>
2023-09-09 12:17:41 -0300

    feat: responsive sidebar finished

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,7 @@
 import { Sidebar } from '@/components/Sidebar'
-import './globals.css'
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
+import './globals.css'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -20,7 +20,9 @@ export default function RootLayout({
       <body className={inter.className}>
         <div className="grid min-h-screen grid-cols-app ">
           <Sidebar />
-          <main className="px-8 pb-12 pt-8">{children}</main>
+          <main className="px-4 pb-12 pt-24 lg:col-start-2 lg:px-8 lg:pb-12 lg:pt-8">
+            {children}
+          </main>
         </div>
       </body>
     </html>
